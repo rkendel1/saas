e1e58fd00ce079da10def79ed0af5e69118a0820
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2025-08-15 11:37:59 -0400

    custom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,24 @@
 import About from "@/components/About";
-import HomeBlogSection from "@/components/Blog/HomeBlogSection";
 import CallToAction from "@/components/CallToAction";
-import Clients from "@/components/Clients";
+
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
-import Faq from "@/components/Faq";
+
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
-import Team from "@/components/Team";
-import Testimonials from "@/components/Testimonials";
-import { getAllPosts } from "@/utils/markdown";
+
+
+
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Play Next.js - SaaS Starter Kit and Boilerplate for Next.js",
-  description: "Free Next.js SaaS Boilerplate and Starter Kit designed and built for SaaS startups. It comes with all necessary integrations, pages, and components you need to launch a feature-rich SaaS websites.",
+  title: "ID8 is a Native AI Ideation Platform",
+  description: "ID8 is a Native AI Ideation Platform built for the success of the solo founder.",
 };
 
 export default function Home() {
-  const posts = getAllPosts(["title", "date", "excerpt", "coverImage", "slug"]);
+
 
   return (
     <main>
@@ -29,12 +28,11 @@ export default function Home() {
       <About />
       <CallToAction />
       <Pricing />
-      <Testimonials />
-      <Faq />
-      <Team />
-      <HomeBlogSection posts={posts} />
+      
+      
+     
       <Contact />
-      <Clients />
+   
     </main>
   );
 }
